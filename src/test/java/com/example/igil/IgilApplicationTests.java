fb168c162026e9356ffd080f6c299f73fc890f21
xxx0211 <bimo211@gmail.com>
2021-06-07 08:05:42 +0300

    Initial commit

---
+++ src/test/java/com/example/igil/IgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.igil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
